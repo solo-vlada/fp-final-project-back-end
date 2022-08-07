53df75916d287615e1ffae31cbc46f21bfe39ca4
sigltech <101203107+sigltech@users.noreply.github.com>
2022-08-07 23:46:23 +0100

    make it better

--- README.md
+++ README.md
@@ -3,4 +3,16 @@
 # Welcome to PreLoved! 
 
 ## Description
-Have you ever wanted to get new clothes but don't know what to do with the old ones 
+Have you ever wanted to get new clothes but don't know what to do with the old ones? Then this is the perfect place for you. Preloved is a website that allows you to search for clothes that you want to get new and get them for free.
+
+## Local Setup instructions
+
+1. run `pipenv install` in the terminal to install all the dependencies
+2. run `pipenv install --dev` to install dev dependancies (if you want to examine the testing results and coverage)
+3. run `pipenv shell` to enter the virtual environment
+4. run `pipenv run dev` to run the development server
+5.
+5. run `pipenv run test` to run the tests
+6. run `pipenv run coverage` to run the coverage report
+
+
--- server_app/__init__.py
+++ server_app/__init__.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 from os import environ
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_cors import CORS
+# from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+#                                unset_jwt_cookies, jwt_required, JWTManager
 
 from .database.db import db
 from .routes.main import main_routes
@@ -9,7 +11,6 @@ from .routes.auth import auth_routes
 
 #load environment variables
 load_dotenv()
-
 database_uri = environ.get('DATABASE_URL')
 
 if 'postgres:' in database_uri:
@@ -21,6 +22,8 @@ app.config.update(
     SQLALCHEMY_DATABASE_URI=database_uri,
     SQLALCHEMY_TRACK_MODIFICATIONS=environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 )
+# app.config["JWT_SECRET_KEY"] = "please-remember-to-change-me"
+# jwt = JWTManager(app)
 
 CORS(app)
 db.app = app
