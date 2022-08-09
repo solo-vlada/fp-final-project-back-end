499fbad6a5420c6fccccf88275363463ca569c27
sigltech <101203107+sigltech@users.noreply.github.com>
2022-08-09 13:05:03 +0100

    new changes from main

--- server_app/models/tables.py
+++ server_app/models/tables.py
@@ -10,7 +10,7 @@ class Clothing(db.Model):
     description = db.Column(db.String(80), nullable=False)
     category = db.Column(db.String(80), nullable=False)
     size = db.Column(db.String(80), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.String(80), db.ForeignKey('user.id'))
     on_offer = db.Column(db.Boolean, default=False, nullable=False)
     images = db.Column(db.String(500), nullable=True)
 
@@ -22,18 +22,16 @@ class Clothing(db.Model):
         self.user_id = user_id
         self.on_offer = on_offer
         self.images = images
-    
-    # def __getitem__(self, item_name, description, category, size, user_id, on_offer, images):
-    #     print (item_name, description, category, size, user_id, on_offer, images)
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+class User(db.Model):
+    id = db.Column(db.String(80), primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(300), nullable=False)
     email = db.Column(db.String(80), nullable=False)
     location = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, username, password, email, location):
+    def __init__(self, id, username, password, email, location):
+        self.id = id
         self.username = username
         self.password = password
         self.email = email
@@ -41,7 +39,7 @@ class User(db.Model, UserMixin):
 
 class Offers(db.Model):
     offer_id = db.Column(db.Integer, primary_key=True)
-    proposer = db.Column(db.ForeignKey('user.id'))
+    proposer = db.Column(db.String(80), db.ForeignKey('user.id'))
     reciever = db.Column(db.ForeignKey('user.id'))
     offer_status = db.Column(db.Boolean, nullable=False)
     offer_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow()) #proposed extra columns for offer table
--- server_app/routes/auth.py
+++ server_app/routes/auth.py
@@ -1,7 +1,8 @@
-from urllib import response
 from flask import Blueprint, jsonify, make_response, request, current_app as app, redirect
+from flask_cors import cross_origin
 from werkzeug.security import generate_password_hash,check_password_hash
 from functools import wraps
+import uuid
 import jwt
 import datetime
 from ..database.db import db
@@ -10,7 +11,7 @@ from flask_login import login_user, logout_user, current_user, login_required, L
 
 auth_routes = Blueprint("auth", __name__)
 
-# Creates a decorator for checking valid json web tokens
+# Creates a decorator for checking valid json web tokens that can be used to limit methods to valid token users
 def token_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
@@ -28,45 +29,29 @@ def token_required(f):
         return f(current_user, *args, **kwargs)
     return decorator
 
-# ********************* get all users *********************
+# Register new user / expects json post handled by frontend
+@auth_routes.route('/register', methods=['POST'])
+@cross_origin()
+def register_user(): 
+    try:
+        content = request.json
+        hashed_password = generate_password_hash(content['password'], method='sha256')
 
-def user_serialiser(user):
-    return {
-        'id': user.id,
-        'username': user.username,
-        'password': user.password,
-        'location': user.location,
-        'email': user.email
-    }
-
-@auth_routes.route('/users', methods=['GET', 'POST'])
-def get_users():
-    if request.method == 'GET':
-
-        # create a variable to hold the users queried from the database
-        all_users = User.query.all()
-
-        # use * to unpack the list of users, map to serialise each user. user_serialiser is a function that serialises each element of the user model
-        return jsonify([*map(user_serialiser, all_users)])  
-
-    elif request.method == 'POST':
-
-        # create a new user object from the request form data
-        new_user = User( 
-            username=request.json['username'],
-            password=generate_password_hash(request.json['password'], method='sha256'),
-            location=request.json['location'],
-            email=request.json['email']
+        new_user = User(
+            id = f'{uuid.uuid1()}',
+            username = content['username'], 
+            password = hashed_password, 
+            location = content['location'], 
+            email = content['email']
         )
+
         db.session.add(new_user)
-        db.session.commit()
-        return jsonify({'message': 'user created successfully'}), 201
+        db.session.commit()   
+        return jsonify({'message': 'registered successfully'}), 201
+    except:
+        return jsonify({'message': 'registration unsuccessful'}), 400
 
-@auth_routes.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-        user = User.query.get(id)
-        return jsonify(user_serialiser(user))
-
+<<<<<<< HEAD
 
 # ********************* verify Login *********************
 
@@ -82,21 +67,78 @@ def load_user(id):
 
 # Login to existing account
 @auth_routes.route('/login', methods=['POST'])
+=======
+# Login to existing account / expects basic auth containing the username and password
+@auth_routes.route('/login', methods=['POST']) 
+>>>>>>> 79fea7b56df85d15e3805c2dd926712f0239619c
 def login_user():
 
+    # Check that login request was sent with basic auth
     auth = request.authorization  
     if not auth or not auth.username or not auth.password: 
-       return make_response('could not verify', 401, {'Authentication': 'login required"'})   
- 
+        return make_response('could not verify basic auth', 401, {'Authentication': 'login required"'})   
+    
     user = User.query.filter_by(username=auth.username).first()  
     if check_password_hash(user.password, auth.password):
-       token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=45)}, app.config['SECRET_KEY'], "HS256")
- 
-       return jsonify({'token' : token})
- 
-    return make_response('could not verify',  401, {'Authentication': '"login required"'})
+    # if user.password == auth.password:
+        token = jwt.encode({'id': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=45)}, app.config['SECRET_KEY'], "HS256"), {"user_id": user.id, "username": user.username}
+
+<<<<<<< HEAD
 
 
 
+=======
+        return jsonify({'token': token}), 200
+     
+    return jsonify('could not verify'), 401
+>>>>>>> 79fea7b56df85d15e3805c2dd926712f0239619c
 
+# Test route reciive all users in json format
+@auth_routes.route('/users', methods=['GET'])
+def get_all_users():
 
+    users = User.query.all()
+    result = []  
+    for user in users:  
+        user_data = {}  
+        user_data['id'] = user.id 
+        user_data['username'] = user.username
+        user_data['password'] = user.password
+        user_data['location'] = user.location
+        user_data['email'] = user.email
+        
+        result.append(user_data)  
+    return jsonify({'users': result})
+
+@auth_routes.route('/msg/<string:user_id>', methods=['GET', 'POST'])
+def messenger_handling(user_id):
+    if request.method == 'GET':
+        try:
+            #  retrieve all messages sent by or too user
+            all_messages = Messages.query.filter(Messages.sender == user_id and Messages.receiver == user_id )
+
+            def message_serializer(message):
+                return {
+                    "message_text": message.message_text,
+                    "sender": message.sender,
+                    "receiver": message.receiver
+                }
+                
+            return jsonify({'Messages': [*map(message_serializer, all_messages)]}), 200
+        except:
+            return jsonify({'Error': 'Cannot retrieve message\'s from non-existent user'}), 404
+    else:
+        try:
+            # expect message in json format with user_id and receiver_id as the sender and recipient
+            content = request.json
+            new_message = Messages(
+                message_text=content['message_text'],
+                sender=content['user_id'],
+                receiver=content['receiver_id']
+            )
+
+            db.session.add(new_message)
+            db.session.commit()
+            return jsonify({'Message sent': new_message.message_text}), 201
+        except:
+            return jsonify({'Error': 'Cannot send message to non-existent user'}), 404
