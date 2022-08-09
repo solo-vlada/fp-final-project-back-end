b1870ddae0973f01388abd8343ae6b7c7054f2e1
Ben <bentidball97@gmail.com>
2022-08-09 23:22:13 +0100

    edit main tests, reworked filters

--- server_app/tests/test_main.py
+++ server_app/tests/test_main.py
@@ -6,13 +6,23 @@ from unittest import mock
 def test_index_get(api):
     resp = api.get('/')
     assert resp.status == '200 OK'
-    print(resp.json)
 
-def test_index_post(api):
-    resp = api.post('/')
+def test_index_get_personal_listings(api):
+    resp = api.get('/?user=1f4d9382-17ff-11ed-8ac3-704d7b3306bd')
     assert resp.status == '200 OK'
-    assert resp.json == {'message': 'added clothing'}
+
+def test_index_get_filter_results(api):
+    resp = api.get('/?category=shirt')
+    assert resp.status == '200 OK'
+
+def test_index_get_personal_listings_filtered(api):
+    resp = api.get('/?user=1f4d9382-17ff-11ed-8ac3-704d7b3306bd&category=shirt')
+    assert resp.status == '200 OK'
 
 def test_index_error(api):
-        resp = api.delete('/')
-        assert resp.status == '405 METHOD NOT ALLOWED'
+    resp = api.post('/')
+    assert resp.status == '405 METHOD NOT ALLOWED'
+
+def test_index_error(api):
+    resp = api.delete('/')
+    assert resp.status == '405 METHOD NOT ALLOWED'
